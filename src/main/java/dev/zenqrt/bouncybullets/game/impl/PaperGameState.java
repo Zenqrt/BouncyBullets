7855a5b76be9d0c26dd86d0f78df4cf9af66e2fe
Walmqrt <46291857+Zenqrt@users.noreply.github.com>
2024-03-07 18:21:46 -0500

    Make PaperGameState abstract

--- src/main/java/dev/zenqrt/bouncybullets/game/impl/PaperGameState.java
+++ src/main/java/dev/zenqrt/bouncybullets/game/impl/PaperGameState.java
@@ -2,14 +2,9 @@ package dev.zenqrt.bouncybullets.game.impl;
 
 import dev.zenqrt.bouncybullets.game.GameState;
 
-public class PaperGameState extends GameState<PaperGameEventHandler> {
+public abstract class PaperGameState extends GameState<PaperGameEventHandler> {
 
     public PaperGameState(PaperGameEventHandler eventHandler) {
         super(eventHandler);
-    }
-
-    @Override
-    public void registerEvents() {
-
     }
 }
